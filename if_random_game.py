a1ed5477ac1dccfaeba1f55941b17e950bb3ae29
Maksim Maiko <14052001max@gmail.com>
2020-03-01 01:24:41 +0300

    init commit

---
+++ if_random_game.py
@@ -0,0 +1,13 @@
+import random
+def randomChoice(x):
+    m=random.randint(1,4)
+    if x==m:
+        print("Победа")
+    elif x>m:
+        print("Введенное число больше загаданного")
+    else:
+        print("Введенное число меньше заданного")
+
+
+x=int(input("Введите число от 1 до 4\n"))
+randomChoice(x)
